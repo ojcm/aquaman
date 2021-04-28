ec9b3d827c41dbfaaabd4a90412222a679e9f6a8
Oliver Muir <ojcmuir@gmail.com>
2021-04-28 17:31:05 +0100

    Format Python files with Black

--- aquaman.py
+++ aquaman.py
@@ -12,47 +12,54 @@ flags.DEFINE_integer("bcm_channel", 17, "BCM channel to read")
 flags.DEFINE_integer("bounce_ms", 300, "Debouncing delay")
 
 GPIO.setmode(GPIO.BCM)
-gauge = prom.Gauge('moisture_bool', 'Whether moisture has been detected or not', ['instance'])
+gauge = prom.Gauge(
+    "moisture_bool", "Whether moisture has been detected or not", ["instance"]
+)
 
 
 def measure(channel, identifier):
-	if GPIO.input(channel):
-		gauge.labels(identifier).set(0)
-		logging.debug("LED off")
-	else:
-		gauge.labels(identifier).set(1)
-		logging.debug("LED on")
+    if GPIO.input(channel):
+        gauge.labels(identifier).set(0)
+        logging.debug("LED off")
+    else:
+        gauge.labels(identifier).set(1)
+        logging.debug("LED on")
 
 
 def debounce_measure(channel, bounce_ms, identifier):
-	# If the sensor is bouncing we want to read the value when it stops bouncing.
-	# This sleep means we read after the GPIO debounce delay so we get the stable value once
-	# the sensor has stopped bouncing.  If the sensor is still bouncing this callback
-	# will be called again.
-	time.sleep(0.001 * bounce_ms)
-	measure(channel, identifier)
+    # If the sensor is bouncing we want to read the value when it stops bouncing.
+    # This sleep means we read after the GPIO debounce delay so we get the stable value once
+    # the sensor has stopped bouncing.  If the sensor is still bouncing this callback
+    # will be called again.
+    time.sleep(0.001 * bounce_ms)
+    measure(channel, identifier)
 
 
 def main(argv):
-	del argv
+    del argv
 
-	callback = lambda channel: debounce_measure(channel, FLAGS.bounce_ms, FLAGS.identifier)
+    callback = lambda channel: debounce_measure(
+        channel, FLAGS.bounce_ms, FLAGS.identifier
+    )
 
-	# Set up pin reads
-	bcm_channel = FLAGS.bcm_channel
-	GPIO.setup(bcm_channel, GPIO.IN)
+    # Set up pin reads
+    bcm_channel = FLAGS.bcm_channel
+    GPIO.setup(bcm_channel, GPIO.IN)
 
-	# measure initial values
-	measure(bcm_channel, FLAGS.identifier)
+    # measure initial values
+    measure(bcm_channel, FLAGS.identifier)
 
-	# measure on pin change
-	GPIO.add_event_detect(bcm_channel, GPIO.BOTH, callback=callback, bouncetime=FLAGS.bounce_ms)
+    # measure on pin change
+    GPIO.add_event_detect(
+        bcm_channel, GPIO.BOTH, callback=callback, bouncetime=FLAGS.bounce_ms
+    )
 
-	logging.info("Starting http server...")
-	prom.start_http_server(8080)
-	logging.info("Started http server on port 8080")
-	while True:
-		time.sleep(0.1)
+    logging.info("Starting http server...")
+    prom.start_http_server(8080)
+    logging.info("Started http server on port 8080")
+    while True:
+        time.sleep(0.1)
 
-if __name__ == '__main__':
-	app.run(main)
+
+if __name__ == "__main__":
+    app.run(main)
--- aquaman_analogue.py
+++ aquaman_analogue.py
@@ -22,67 +22,71 @@ flags.DEFINE_string("id7", None, "Identifier used in metric labels for channel 7
 flags.DEFINE_integer("poll_ms", 1000, "Frequency of polls of sensors")
 
 id_getter = {
-	MCP.P0: lambda: FLAGS.id0,
-	MCP.P1: lambda: FLAGS.id1,
-	MCP.P2: lambda: FLAGS.id2,
-	MCP.P3: lambda: FLAGS.id3,
-	MCP.P4: lambda: FLAGS.id4,
-	MCP.P5: lambda: FLAGS.id5,
-	MCP.P6: lambda: FLAGS.id6,
-	MCP.P7: lambda: FLAGS.id7,
+    MCP.P0: lambda: FLAGS.id0,
+    MCP.P1: lambda: FLAGS.id1,
+    MCP.P2: lambda: FLAGS.id2,
+    MCP.P3: lambda: FLAGS.id3,
+    MCP.P4: lambda: FLAGS.id4,
+    MCP.P5: lambda: FLAGS.id5,
+    MCP.P6: lambda: FLAGS.id6,
+    MCP.P7: lambda: FLAGS.id7,
 }
 
 VOLTAGE_MIN = 0.5  # When moisture is 100%
 VOLTAGE_MAX = 3.3  # When moisture is 0%
 
-class MoistureMeter():
-	def __init__(self, identifier, chan, gauge):
-		self.identifier = identifier
-		self.chan = chan
-		self.gauge = gauge
 
-	def measure(self):
-		logging.debug(self.identifier + ' ADC Voltage: ' + str(self.chan.voltage) + 'V')
-		perc = (VOLTAGE_MAX - self.chan.voltage) / (VOLTAGE_MAX - VOLTAGE_MIN)
-		logging.debug(self.identifier + ' moisture: ' + "{:.0%}".format(perc))
-		if perc < 0 or perc > 1:
-			logging.warning(self.identifier + ' voltage out of range :' + str(self.chan.voltage))
-		self.gauge.labels(self.identifier).set(perc)
+class MoistureMeter:
+    def __init__(self, identifier, chan, gauge):
+        self.identifier = identifier
+        self.chan = chan
+        self.gauge = gauge
+
+    def measure(self):
+        logging.debug(self.identifier + " ADC Voltage: " + str(self.chan.voltage) + "V")
+        perc = (VOLTAGE_MAX - self.chan.voltage) / (VOLTAGE_MAX - VOLTAGE_MIN)
+        logging.debug(self.identifier + " moisture: " + "{:.0%}".format(perc))
+        if perc < 0 or perc > 1:
+            logging.warning(
+                self.identifier + " voltage out of range :" + str(self.chan.voltage)
+            )
+        self.gauge.labels(self.identifier).set(perc)
 
 
 def main(argv):
-	del argv
+    del argv
 
-	# Setup up board
-	spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-	cs = digitalio.DigitalInOut(board.CE0)
-	mcp = MCP.MCP3008(spi, cs)
+    # Setup up board
+    spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+    cs = digitalio.DigitalInOut(board.CE0)
+    mcp = MCP.MCP3008(spi, cs)
 
-	# Setup up metrics
-	gauge = prom.Gauge('moisture_percent', 'Moisture percentage', ['sensor_id'])
-	logging.info("Starting http server...")
-	prom.start_http_server(8080)
-	logging.info("Started http server on port 8080")
+    # Setup up metrics
+    gauge = prom.Gauge("moisture_percent", "Moisture percentage", ["sensor_id"])
+    logging.info("Starting http server...")
+    prom.start_http_server(8080)
+    logging.info("Started http server on port 8080")
 
-	meters = []
-	for pin, getter in id_getter.items():
-		id = getter()
-		if id:
-			logging.info("Measuring pin " + str(pin) + " with id " + id)
-			meter = MoistureMeter(id, AnalogIn(mcp, pin), gauge)
-			meters.append(meter)
-		else:
-			logging.info("Not measuring pin " + str(pin))
+    meters = []
+    for pin, getter in id_getter.items():
+        id = getter()
+        if id:
+            logging.info("Measuring pin " + str(pin) + " with id " + id)
+            meter = MoistureMeter(id, AnalogIn(mcp, pin), gauge)
+            meters.append(meter)
+        else:
+            logging.info("Not measuring pin " + str(pin))
 
-	if not meters:
-		logging.error("no pins are being measured")
-		return
+    if not meters:
+        logging.error("no pins are being measured")
+        return
 
-	while True:
-		for m in meters:
-			m.measure()
-		logging.debug("Sleeping for " + str(FLAGS.poll_ms) + "ms")
-		time.sleep(0.001 * FLAGS.poll_ms)
+    while True:
+        for m in meters:
+            m.measure()
+        logging.debug("Sleeping for " + str(FLAGS.poll_ms) + "ms")
+        time.sleep(0.001 * FLAGS.poll_ms)
 
-if __name__ == '__main__':
-	app.run(main)
+
+if __name__ == "__main__":
+    app.run(main)
