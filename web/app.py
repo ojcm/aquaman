a5052ead69a1fe53d87982b56ea14c8b7b6eea3b
ojcm <ojcmuir@gmail.com>
2021-05-04 22:01:30 +0100

    Get and set config file

--- web/app.py
+++ web/app.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+
+from werkzeug.datastructures import MultiDict
+from absl import app, flags, logging
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
@@ -5,48 +9,89 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import json
 
+FLAGS = flags.FLAGS
+flags.DEFINE_string("config_file_path", None, "Path of config file to read/write")
+flags.mark_flag_as_required("config_file_path")
 
-app = Flask(__name__, template_folder='.')
+flask_app = Flask(__name__, template_folder=".")
+Bootstrap(flask_app)  # Flask-Bootstrap requires this line
 
-# Flask-WTF requires an encryption key - the string can be anything
-app.config['SECRET_KEY'] = 'secret_key_lkjbdsfasojabdl'
-
-# Flask-Bootstrap requires this line
-Bootstrap(app)
 
 class IdentifiersForm(FlaskForm):
-    id1 = StringField('Sensor 1 ID')
-    id2 = StringField('Sensor 2 ID')
-    id3 = StringField('Sensor 3 ID')
-    id4 = StringField('Sensor 4 ID')
-    id5 = StringField('Sensor 5 ID')
-    id6 = StringField('Sensor 6 ID')
-    id7 = StringField('Sensor 7 ID')
-    id8 = StringField('Sensor 8 ID')
-    submit = SubmitField('Submit')
+    id1 = StringField("Sensor 1 ID")
+    id2 = StringField("Sensor 2 ID")
+    id3 = StringField("Sensor 3 ID")
+    id4 = StringField("Sensor 4 ID")
+    id5 = StringField("Sensor 5 ID")
+    id6 = StringField("Sensor 6 ID")
+    id7 = StringField("Sensor 7 ID")
+    id8 = StringField("Sensor 8 ID")
+    submit = SubmitField("Submit")
 
 
-@app.route('/', methods=['GET', 'POST'])
+class ConfigFile:
+    def __init__(self, path):
+        self.path = path
+        self.config = {}
+        self.read()
+
+    def read(self):
+        logging.debug("reading file " + self.path)
+        with open(self.path) as f:
+            self.config = json.load(f)
+
+    def write(self):
+        logging.debug("writing to " + self.path)
+        with open(self.path, "w") as f:
+            json.dump(self.config, f)
+
+    def set_config(self, config):
+        logging.debug("setting config: " + str(config))
+        self.config = config
+
+    def string(self):
+        return json.dumps(self.config)
+
+
+@flask_app.route("/", methods=["GET", "POST"])
 def index():
-    # TODO: populate form with data from config file
-    form = IdentifiersForm()
-    identifiers = {}
+    config = flask_app.config["aquaman_config"]
+    form = IdentifiersForm(
+        meta={"csrf": False},  # Disable CSRF (not required at this time)
+        id1=config.config["id1"],
+        id2=config.config["id2"],
+        id3=config.config["id3"],
+        id4=config.config["id4"],
+        id5=config.config["id5"],
+        id6=config.config["id6"],
+        id7=config.config["id7"],
+        id8=config.config["id8"],
+    )
+
     if form.validate_on_submit():
         identifiers = {
-            'id1': form.id1.data,
-            'id2': form.id2.data,
-            'id3': form.id3.data,
-            'id4': form.id4.data,
-            'id5': form.id5.data,
-            'id6': form.id6.data,
-            'id7': form.id7.data,
-            'id8': form.id8.data,
+            "id1": form.id1.data,
+            "id2": form.id2.data,
+            "id3": form.id3.data,
+            "id4": form.id4.data,
+            "id5": form.id5.data,
+            "id6": form.id6.data,
+            "id7": form.id7.data,
+            "id8": form.id8.data,
         }
-    
-    # TODO: write config to file
+        config.set_config(identifiers)
+        config.write()
+        flask_app.config["aquaman_config"] = config
 
-    j = json.dumps(identifiers)
-    return render_template('index.html', form=form, json=j)
+    return render_template("index.html", form=form, config=config.string())
 
-if __name__ == '__main__':
-    app.run()
+
+def main(argv):
+    del argv
+    conf = ConfigFile(FLAGS.config_file_path)
+    flask_app.config["aquaman_config"] = conf
+    flask_app.run()
+
+
+if __name__ == "__main__":
+    app.run(main)
